7b2d8db93928fbd1425dbbc120548d15587d4f3c
myrosvas <myrosvas@gmail.com>
2019-09-24 21:18:28 +0300

    redux added

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import counter from './reducers'
+
+const store = createStore(counter)
 
 ReactDOM.render(
+  <Provider store={ store }>
     <App />
+  </Provider>
   , document.querySelector('#root'));
+
+  // ReactDOM.render(
+  //   <Provider store={ store }>
+  //     <App />
+  //   </Provider>
+  //   , document.querySelector('#root'));

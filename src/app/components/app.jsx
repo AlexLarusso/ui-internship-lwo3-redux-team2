1736560ced3e60388bcc114049a3019e4108c2a6
mvasylykiv <mvasylykiv@griddynamics.com>
2019-09-27 15:08:19 +0300

    redux fixed

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import style from './app.scss';
+// import React from 'react';
+// import style from './app.scss';
 
-const App = () => {
-  console.log(style);
-  return (
-    <div className={style.test}>
-      <p className={style.testHello}>React here!</p>
-      <p className="test">React here!</p>
-      <img src={require('../../assets/logo.png')} alt="pictur" />
-    </div>
-  );
-};
+// const App = () => {
+//   console.log(style);
+//   return (
+//     <div className={style.test}>
+//       <p className={style.testHello}>React here!</p>
+//       <p className="test">React here!</p>
+//       <img src={require('../../assets/logo.png')} alt="pictur" />
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import CounterContainer from '../containers/CounterContainer';
 
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <CounterContainer />
+    );
+  }
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // Redux test component:
 // import React, { Component } from 'react';
